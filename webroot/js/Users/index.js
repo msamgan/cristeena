e4ea3ce2fa3299ddf4bf3e2b6e160e25f484a3e0
samgan-khan <mohdsamgankhan@gmail.com>
2018-08-31 16:55:24 +0530

    deleteUser finction seperate in index

--- webroot/js/Users/index.js
+++ webroot/js/Users/index.js
@@ -3,31 +3,43 @@ define([
     'datatables',
     'methods'
 ], function ($, DataTable, methods) {
+
+    /**
+     * js for deleting the user.
+     */
+    function deleteUser() {
+        $('.delete-user').click(function() {
+            swal({
+                title: "Are you sure you want to delete user?",
+                text: "Once deleted, you will not be able to recover this user data",
+                buttons: true,
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    $.get($(this).data('path')).done(function (response) {
+                        response = methods.toArray(response);
+                        if (response['status']) {
+                            methods.notify(
+                                response['title'],
+                                response['message'],
+                                'success'
+                            );
+                            methods.reload(2);
+                        } else {
+                            methods.notify(response['title'], response['message'], 'error');
+                        }
+                    });
+                }
+            });
+        });
+    }
+
     /**
      * fetching all the users in the system.
      */
     $('#user-list-table').DataTable({
-        "fnDrawCallback": function( settings ) {
-            $('.delete-user').click(function() {
-                swal({
-                    title: "Are you sure you want to delet user?",
-                    text: "Once deleted, you will not be able to recover this user data",
-                    buttons: true,
-                    dangerMode: true,
-                }).then((willDelete) => {
-                    if (willDelete) {
-                        $.get($(this).data('path')).done(function (response) {
-                            response = methods.toArray(response);
-                            if (response['status']) {
-                                methods.notify(response['title'], response['message'], 'success');
-                                methods.reload(2);
-                            } else {
-                                methods.notify(response['title'], response['message'], 'error');
-                            }
-                        });
-                    }
-                });
-            });
+        "fnDrawCallback": function() {
+            deleteUser();
         },
         destroy: true,
         "ajax": "/api/users/index",
@@ -43,27 +55,8 @@ define([
      * fetching all the admins in the system.
      */
     $('#admin-list-table').DataTable({
-        "fnDrawCallback": function( settings ) {
-            $('.delete-user').click(function() {
-                swal({
-                    title: "Are you sure you want to delet user?",
-                    text: "Once deleted, you will not be able to recover this user data",
-                    buttons: true,
-                    dangerMode: true,
-                }).then((willDelete) => {
-                    if (willDelete) {
-                        $.get($(this).data('path')).done(function (response) {
-                            response = methods.toArray(response);
-                            if (response['status']) {
-                                methods.notify(response['title'], response['message'], 'success');
-                                methods.reload(2);
-                            } else {
-                                methods.notify(response['title'], response['message'], 'error');
-                            }
-                        });
-                    }
-                });
-            });
+        "fnDrawCallback": function() {
+            deleteUser();
         },
         destroy: true,
         "ajax": "/api/users/admins",
