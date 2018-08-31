dc1fd32a76fea4c24d5939f4e59401c6a2f33907
samgan-khan <mohdsamgankhan@gmail.com>
2018-08-31 17:30:11 +0530

    space between operator

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -50,7 +50,7 @@ class User extends Entity
 
     /**
      * @param $password
-     * @return mixed
+     * @return bool|string
      */
     protected function _setPassword($password)
     {
@@ -88,7 +88,7 @@ class User extends Entity
      */
     private function _adminEdit()
     {
-        return '<a class="btn-info btn-xs" href="/admin/users/edit/'. $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
+        return '<a class="btn-info btn-xs" href="/admin/users/edit/' . $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
     }
 
     /**
@@ -96,7 +96,7 @@ class User extends Entity
      */
     private function _directorEditAdmin()
     {
-        return '<a class="btn-info btn-xs" href="/director/admins/edit/'. $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
+        return '<a class="btn-info btn-xs" href="/director/admins/edit/' . $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
     }
 
     /**
@@ -104,7 +104,7 @@ class User extends Entity
      */
     private function _directorEditUser()
     {
-        return '<a class="btn-info btn-xs" href="/director/users/edit/'. $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
+        return '<a class="btn-info btn-xs" href="/director/users/edit/' . $this->_properties['slug'] .'" title="Edit user" ><i class="lnr lnr-pencil" ></i></a>';
     }
 
     /**
@@ -112,6 +112,6 @@ class User extends Entity
      */
     private function _delete()
     {
-        return '<a class="btn-danger btn-xs delete-user" style="cursor: pointer; margin-left:5px;" data-path="/api/users/delete/'. $this->_properties['id'] .'" title="Delete user" ><i class="lnr lnr-trash" ></i></a>';
+        return '<a class="btn-danger btn-xs delete-user" style="cursor: pointer; margin-left:5px;" data-path="/api/users/delete/' . $this->_properties['id'] .'" title="Delete user" ><i class="lnr lnr-trash" ></i></a>';
     }
 }
